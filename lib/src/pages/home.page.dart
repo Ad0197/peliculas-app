a10513c958729cb7335d8cf31a73f066a02c3cea
Adrison Gomez <adrison.gomez@hotmail.com>
2020-01-02 16:27:53 -0400

    Swipper

--- lib/src/pages/home.page.dart
+++ lib/src/pages/home.page.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
   static String pageName = '/';
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: (){},),
+          ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
     return Container(
-      child: Scaffold(body: SafeArea(child: Text("Hola Mundo!!!!")),),
+      height: 300.0,
+      width: double.infinity,
+      child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          itemWidth: 300.0,
+          // pagination: new SwiperPagination(),
+          layout: SwiperLayout.STACK,
+          // control: new SwiperControl(),
+        ),
     );
   }
 }
