e888aabab17a78008a6e8e270972c334b75598e3
Adrison Gomez <adrison.gomez@hotmail.com>
2020-01-02 23:54:51 -0400

    Trabjando con el customScrollView y el SliverAppBar

--- lib/src/pages/pelicula.detalles.dart
+++ lib/src/pages/pelicula.detalles.dart
@@ -18,4 +18,23 @@ class PeliculaDetalle extends StatelessWidget {
 }
 
 Widget _crearAppBar(Pelicula pelicula) {
+  return SliverAppBar(
+    elevation: 2.0,
+    backgroundColor: Colors.indigo,
+    expandedHeight: 200.0,
+    floating: false,
+    pinned: true,
+    flexibleSpace: FlexibleSpaceBar(
+      centerTitle: true,
+      title: Text(
+        pelicula.title,
+        style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+      background: FadeInImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(pelicula.getBackgroundImgUrl()),
+          placeholder: AssetImage('assets/img/loading.gif'),
+        ),
+    ),
+  );
 }
